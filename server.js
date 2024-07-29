3826131870a157d69a33df58bc338091733969cb
Pratik Pawar <pawarpratik7349@gmail.com>
2024-07-29 12:51:19 +0530

    Add root and fallback routes

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 import cors from 'cors';
 import express from 'express';
-const app = express();
+import morgan from 'morgan';
 import './db.js';
 import authRoutes from './routes/authRoutes.js';
 import taskRouter from './routes/taskRoutes.js';
-import morgan from 'morgan';
 
+const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+
+// Define routes
 app.use('/auth', authRoutes);
 app.use('/task', taskRouter);
 
-// localhost:4000/auth/register
+// Define the root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Task Manager API');
+});
 
-const port = 4000;
+// Fallback route for undefined paths
+app.use((req, res, next) => {
+  res.status(404).send('Route not found');
+});
 
+// Start the server
 app.listen(port, () => {
-	console.log(`server is running on port`, port);
+  console.log(`Server is running on port ${port}`);
 });
